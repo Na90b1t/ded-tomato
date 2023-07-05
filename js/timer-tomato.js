bf8b628e62a67deecee2f61cd294ffd98d3deea5
Na90b1t <na90b1t@mail.ru>
2023-07-05 12:05:50 +0300

    Фикс рекурсивно наращивающегося счетчика. Причина была в перенавешивании слушателей событий.

--- js/timer-tomato.js
+++ js/timer-tomato.js
@@ -77,65 +77,65 @@ const dragAndDrop = () => {
 	console.log('dragAndDrop');
 
 	cardTomato.setAttribute('draggable', 'true'); // вкл перетаскивание
-
-	const dragStart = function () {
-		// console.log('dragStart');
-		setTimeout(() => { // (хак) таймаут нужен, чтобы успеть зацепить перетаскиваемый объект до его скрытия классом hide
-			cardTomato.classList.add('hide'); // добавляем класс для скрытия объекта в начальной точке, после события dragstart (перетаскивания), чтобы был виден только перетаскивающийся объект
-		}, 0);
-	};
-
-	const dragEnd = function () {
-		// console.log('dragEnd');
-		cardTomato.classList.remove('hide'); // удаляем скрывающий класс, чтобы снова был виден перетаскивающийся объект в начальной позиции
-	};
-
-	const dragOver = function (evt) { // передаем событие (без этого не сработает dragDrop)
-		// console.log('dragOver');
-		evt.preventDefault(); // отменяем действие по умолчанию (без этого не сработает dragDrop)
-	};
-
-	const dragEnter = function (evt) { // передаем событие (? для безопасности)
-		// console.log('dragEnter');
-		evt.preventDefault(); // ? для безопасности
-		this.classList.add('hovered'); // Подсветка ячейки при наведении перетаскиваемого объекта на нее
-	};
-
-	const dragLeave = function () {
-		// console.log('dragLeave');
-		this.classList.remove('hovered'); // Удаляем подсветку ячейки, когда объект покидает пределы
-	};
-
-	const dragDrop = function () {
-		console.log('dragDrop');
-		this.classList.remove('hovered'); // убираем подсветку ячейки, так как после перетаскивания объекта в ячейку она тут больше не нужна
-
-		countDrop();
-	};
-
-	const countDrop = function () {
-		console.group('countDrop')
-		// console.log('countDrop');
-		console.log('countDrop -> countTomato : ', countTomato);
-		console.groupEnd;
-		
-		countTomato = ++countTomato;
-		countBasket.innerHTML = countTomato;
-		
-		/* localStorage.setItem('countTomato', countTomato); // будет для сохранения счетчика при обновлении (пока недореализованно)
-		storageCountTomato = localStorage.getItem('countTomato');
-		console.log('storageCountTomato', storageCountTomato); */
-
-		cardTomato.setAttribute('draggable', 'false'); // выкл возможность перетаскивать помидоры после одного перетаскивания
-		startSeconds = startSecondsValue;
-		prepareTime(); // форматируем вид вывода секунд
-	};
-
-	cardTomato.addEventListener('dragstart', dragStart); // (первое) событие происходящее в момент перетаскивания
-	cardTomato.addEventListener('dragend', dragEnd); // событие происходящее в момент окончания перетаскивания
-
-	cellBasket.addEventListener('dragover', dragOver); // событие постоянно срабатывает, когда оно в пределах элемента, в который ее можно дропнуть. Нужно для срабатываения другого эвента dragDrop
-	cellBasket.addEventListener('dragenter', dragEnter); // событие срабатывает один раз, когда оно впределах элемента, в который ее можно дропнуть
-	cellBasket.addEventListener('dragleave', dragLeave); // событие срабатывает один раз, когда оно покидает пределы элемента, в который ее можно было дропнуть
-	cellBasket.addEventListener('drop', dragDrop); // событие срабатывает при сбросе в допустимую область
 };
+
+const dragStart = function () {
+	// console.log('dragStart');
+	setTimeout(() => { // (хак) таймаут нужен, чтобы успеть зацепить перетаскиваемый объект до его скрытия классом hide
+		cardTomato.classList.add('hide'); // добавляем класс для скрытия объекта в начальной точке, после события dragstart (перетаскивания), чтобы был виден только перетаскивающийся объект
+	}, 0);
+};
+
+const dragEnd = function () {
+	// console.log('dragEnd');
+	cardTomato.classList.remove('hide'); // удаляем скрывающий класс, чтобы снова был виден перетаскивающийся объект в начальной позиции
+};
+
+const dragOver = function (evt) { // передаем событие (без этого не сработает dragDrop)
+	// console.log('dragOver');
+	evt.preventDefault(); // отменяем действие по умолчанию (без этого не сработает dragDrop)
+};
+
+const dragEnter = function (evt) { // передаем событие (? для безопасности)
+	// console.log('dragEnter');
+	evt.preventDefault(); // ? для безопасности
+	this.classList.add('hovered'); // Подсветка ячейки при наведении перетаскиваемого объекта на нее
+};
+
+const dragLeave = function () {
+	// console.log('dragLeave');
+	this.classList.remove('hovered'); // Удаляем подсветку ячейки, когда объект покидает пределы
+};
+
+const dragDrop = function (e) {
+	console.log('dragDrop', e);
+	this.classList.remove('hovered'); // убираем подсветку ячейки, так как после перетаскивания объекта в ячейку она тут больше не нужна
+
+	countDrop(); // !here
+};
+
+const countDrop = function () {
+	console.group('countDrop')
+	// console.log('countDrop');
+	console.log('countDrop -> countTomato : ', countTomato);
+	console.groupEnd;
+	
+	countTomato = ++countTomato;
+	countBasket.innerHTML = countTomato;
+	
+	/* localStorage.setItem('countTomato', countTomato); // будет для сохранения счетчика при обновлении (пока недореализованно)
+	storageCountTomato = localStorage.getItem('countTomato');
+	console.log('storageCountTomato', storageCountTomato); */
+
+	cardTomato.setAttribute('draggable', 'false'); // выкл возможность перетаскивать помидоры после одного перетаскивания
+	startSeconds = startSecondsValue;
+	prepareTime(); // форматируем вид вывода секунд
+};
+
+cardTomato.addEventListener('dragstart', dragStart); // (первое) событие происходящее в момент перетаскивания
+cardTomato.addEventListener('dragend', dragEnd); // событие происходящее в момент окончания перетаскивания
+
+cellBasket.addEventListener('dragover', dragOver); // событие постоянно срабатывает, когда оно в пределах элемента, в который ее можно дропнуть. Нужно для срабатываения другого эвента dragDrop
+cellBasket.addEventListener('dragenter', dragEnter); // событие срабатывает один раз, когда оно впределах элемента, в который ее можно дропнуть
+cellBasket.addEventListener('dragleave', dragLeave); // событие срабатывает один раз, когда оно покидает пределы элемента, в который ее можно было дропнуть
+cellBasket.addEventListener('drop', dragDrop); // событие срабатывает при сбросе в допустимую область
